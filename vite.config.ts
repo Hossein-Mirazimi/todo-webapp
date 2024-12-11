86267cc6852fbd97b5145c80a36e12ed18c34286
Hossein Mirazimi <h.mirazimi@alibaba.ir>
2024-12-11 16:54:15 +0330

    feat: add compression resource plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    Pages({
-      resolver: 'vue',
-      dirs: './src/pages',
-      importMode: 'async',
-    })
-  ],
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve';
+  const buildPlugins: Plugin[] = [
+      // Compression({}),
+      // Compression({ algorithm: 'brotliCompress', ext: '.br' }),
+  ]
+  return {
+    plugins: [
+      vue(),
+      Pages({
+        resolver: 'vue',
+        dirs: './src/pages',
+        importMode: 'async',
+      }),
+      !isDev && buildPlugins
+    ],
+  }
 })
